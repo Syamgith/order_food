1f02afc51a8a09c5155e4b2c2816564d8e576d9c
syamgith <syamgithksudheesh@gmail.com>
2020-07-28 17:00:44 +0530

    ui

--- lib/home.dart
+++ lib/home.dart
@@ -4,7 +4,22 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Home'),
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text('Home'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            title: Text('MY CART'),
+          ),
+        ],
+      ),
+      body: Text('HOME'),
     );
   }
 }
